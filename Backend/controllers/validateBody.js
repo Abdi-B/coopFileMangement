c44c533dfa36347142df870feed9c07ff604df29
Abdi Bacha <contactabdi.bacha@gmail.com>
2024-03-16 23:47:45 +0300

    update validation

--- Backend/controllers/validateBody.js
+++ Backend/controllers/validateBody.js
@@ -9,5 +9,13 @@ exports.validatePost = (req, res, next) => {
     }
     next();
 }
-
+exports.ValidateUser = (req, res, next) => {
+    if(!req.body.email && !req.body.password) {
+        return res.status(400).json({
+            status: 'failed',
+            message: 'Not a valid login Data'
+        });
+    }
+    next(); 
+}
 
