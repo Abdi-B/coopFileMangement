37e2542ee4a288d3c8f6dcc9779d3a00988b61b8
Abdi Bacha <contactabdi.bacha@gmail.com>
2024-03-16 11:46:16 +0300

    update validateBody

--- Backend/controllers/validateBody.js
+++ Backend/controllers/validateBody.js
@@ -1,8 +1,5 @@
 
-<<<<<<< HEAD
-=======
 
->>>>>>> d605fc2ba8f9820bd28da1e0d52295d8f2fda4d4
 exports.validatePost = (req, res, next) => {
     if(!req.body.title || !req.body.content) {
         return res.status(400).json({
