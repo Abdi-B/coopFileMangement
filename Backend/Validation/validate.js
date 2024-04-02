3955008016edea56eb989b473e870e0ca81fc9b8
Abdi Bacha <contactabdi.bacha@gmail.com>
2024-04-02 22:17:54 +0300

    update

--- Backend/Validation/validate.js
+++ Backend/Validation/validate.js
@@ -20,6 +20,7 @@ exports.validateUser = (req, res, next) => {
 };
 
 
+
 exports.validateLoginUser = (req, res, next) => {
     if(!req.body.email &&  !req.body.password) {
         return res.status(400).json({
