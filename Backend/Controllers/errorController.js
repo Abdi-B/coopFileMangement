719f8eaf58b015d0f56bb344ffb86255e0518db6
Abdi Bacha <contactabdi.bacha@gmail.com>
2024-04-03 09:50:47 +0300

    update

--- Backend/Controllers/errorController.js
+++ Backend/Controllers/errorController.js
@@ -1,11 +1,25 @@
 
+
+
+
 const globalErrorHandler = (error, req, res, next)=>{
     error.statusCode = error.StatusCode || 500;
     error.status = error.status || 'error';
-    res.status(error.statusCode).json({
-        status: error.status,
-        message: error.message
-    });
+    if(process.env.Node_ENVT = "development") {
+        res.status(error.statusCode).json({
+            status: error.status,
+            message: error.message,
+            stackTrace: error.stack,
+            error: error 
+    
+        });
+    } else if(process.env.Node_ENVT = "production"){
+        res.status(error.statusCode).json({
+            status: error.status,
+            message: error.message,
+    
+        });
+    }
 };
 
 module.exports = globalErrorHandler;
