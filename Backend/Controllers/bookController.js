d7f8f4238756677a372c35eb41c59d93ae5a0a44
Abdi Bacha <contactabdi.bacha@gmail.com>
2024-04-27 12:17:41 +0300

    update

--- Backend/Controllers/bookController.js
+++ Backend/Controllers/bookController.js
@@ -2,7 +2,7 @@
 
 const allBooks = (req, res) => {
 
-  console.log(req.body)
+//   console.log(req.body)
   const {category, author, title} = req.body
   res.status(200).json({
     status: "success",
