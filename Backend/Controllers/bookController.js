eb8d7e79618efe537cdb3de15c91962cf5cca07d
Abdi Bacha <contactabdi.bacha@gmail.com>
2024-04-27 12:19:30 +0300

    update

--- Backend/Controllers/bookController.js
+++ Backend/Controllers/bookController.js
@@ -1,17 +1,18 @@
+const asyncErrorHandler = require("../Utils/asyncErrorHandler")
 
 
-const allBooks = (req, res) => {
+const allBooks = asyncErrorHandler((req, res) => {
 
-//   console.log(req.body)
-  const {category, author, title} = req.body
-  res.status(200).json({
-    status: "success",
-     category, 
-     author, 
-     title
-  })
-
-}
+    //   console.log(req.body)
+      const {category, author, title} = req.body
+      res.status(200).json({
+        status: "success",
+         category, 
+         author, 
+         title
+      })
+    
+    })
 
 module.exports = {
     allBooks
