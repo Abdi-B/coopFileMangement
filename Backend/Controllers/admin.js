6e1321b0ce3bc5a020d439c6dcedbfbcd4ee2fb5
Abdi Bacha <contactabdi.bacha@gmail.com>
2024-05-02 18:14:15 +0300

    update

--- Backend/Controllers/admin.js
+++ Backend/Controllers/admin.js
@@ -1,7 +1,13 @@
 
 
 const admin = (req, res) => {
-  
+  try {
+    
+  } catch (error) {
+    res.status(500).json({
+        message: "admin page error"
+    })
+  }
 };
 
 module.exports = {admin};
