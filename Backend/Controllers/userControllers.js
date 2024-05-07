415b2fbbbfd828a4535623764f8e027b5234ea84
Abdi Bacha <contactabdi.bacha@gmail.com>
2024-05-07 23:22:55 +0300

    update

--- Backend/Controllers/userControllers.js
+++ Backend/Controllers/userControllers.js
@@ -3,7 +3,6 @@ const util = require('util')
 const bcrypt = require('bcryptjs')
 
 
-
 const User = require('../Models/userModel');
 const asyncErrorHandler = require('../Utils/asyncErrorHandler');
 const customError = require('../Utils/customError');
