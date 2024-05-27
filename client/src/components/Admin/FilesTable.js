b7fd8bbc5637de9a4f34f55c7d95ea9df4e4f130
Abdi Bacha <contactabdi.bacha@gmail.com>
2024-05-27 22:45:53 +0300

    update

--- client/src/components/Admin/FilesTable.js
+++ client/src/components/Admin/FilesTable.js
@@ -1,16 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import { DataGrid } from '@mui/x-data-grid';
-import { Box, Card, Stack, Typography } from '@mui/material';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import { Box, Card, Stack, Typography } from "@mui/material";
+import axios from "axios";
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'department', headerName: 'Department', width: 170 },
-  { field: 'subDepartment', headerName: 'Sub-Department', width: 200 },
-  { field: 'filename', headerName: 'Filename', width: 150 },
-  { field: 'size', headerName: 'Size in kb', type: 'number', width: 90 },
-  { field: 'createdAt', headerName: 'Uploaded At', width: 150 },
-  
+  { field: "id", headerName: "ID", width: 70 },
+  { field: "department", headerName: "Department", width: 170 },
+  { field: "subDepartment", headerName: "Sub-Department", width: 200 },
+  { field: "filename", headerName: "Filename", width: 150 },
+  { field: "size", headerName: "Size in kb", type: "number", width: 90 },
+  { field: "createdAt", headerName: "Uploaded At", width: 150 },
 ];
 
 export default function FilesTable() {
@@ -18,7 +17,7 @@ export default function FilesTable() {
 
   useEffect(() => {
     axios
-      .get('http://localhost:3001/read/getAdminFiles')
+      .get("http://localhost:3001/read/getAdminFiles")
       .then((res) => {
         // console.log(res.data.allFiles);
         setFiles(res.data.allFiles);
@@ -31,35 +30,35 @@ export default function FilesTable() {
     department: file.department,
     subDepartment: file.subDepartment,
     filename: file.fileName,
-    size: (file.size)/1000,
-    createdAt: file.createdAt.substring(0, 10)
+    size: file.size / 1000,
+    createdAt: file.createdAt.substring(0, 10),
   }));
 
   return (
     <Stack
-        style={{
-          height: 400,
-          width: '100%',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          margin: 20,
-        }}
+      style={{
+        height: 400,
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        margin: 20,
+      }}
     >
-      <Typography variant='h6'>Files Table</Typography>
-        <Card sx={{ width: '65%' }}>
-          <DataGrid
-            rows={rows}
-            columns={columns}
-            initialState={{
-              pagination: {
-                paginationModel: { page: 0, pageSize: 5 },
-              },
-            }}
-            pageSizeOptions={[5, 10]}
-            checkboxSelection
-          />
-        </Card>
+      <Typography variant="h6">Files Table</Typography>
+      <Card sx={{ width: "65%" }}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: { page: 0, pageSize: 5 },
+            },
+          }}
+          pageSizeOptions={[5, 10]}
+          checkboxSelection
+        />
+      </Card>
     </Stack>
   );
 }
