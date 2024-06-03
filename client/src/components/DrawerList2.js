31d3be7e52cb4e00fabb69f2bfdf76c63ff91873
Abdi Bacha <contactabdi.bacha@gmail.com>
2024-06-03 11:01:56 +0300

    update

--- client/src/components/DrawerList2.js
+++ client/src/components/DrawerList2.js
@@ -1,35 +1,54 @@
-import React, { useEffect, useState } from 'react';
-import { makeStyles } from '@material-ui/styles';
-import { Container, Drawer, List, ListItem, ListItemText, Typography, ListItemButton, createTheme, Stack, Collapse } from '@mui/material';
-import axios from 'axios';
-import { Link, useLocation } from 'react-router-dom';
-import Navbar from './Navbar';
-import { useAuthContext } from '../hooks/useAuthContext';
-import useMediaQuery from '@mui/material/useMediaQuery';
-import { useTheme } from '@mui/material/styles';
+import React, { useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/styles";
+import {
+  Container,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  ListItemButton,
+  createTheme,
+  Stack,
+  Collapse,
+  Box,
+  Divider,
+} from "@mui/material";
 
-const drawerHeight = '82vh';
+import axios from "axios";
+import { Link, useLocation } from "react-router-dom";
+import Navbar from "./Navbar";
+import { useAuthContext } from "../hooks/useAuthContext";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useTheme } from "@mui/material/styles";
+
+const drawerHeight = "82vh";
 
 const theme = createTheme();
 const useStyles = makeStyles({
   root: {
-    display: 'flex',
+    display: "flex",
+    // padding: "3px",
+    backgroundColor: "gray",
+    // width: "100px",
   },
   drawer: {
-    width: '30%',
+    // width: "15%",
   },
-  drawContainer: {
-    overflowY: 'auto',
+  drawBox: {
+    overflowY: "auto",
     height: drawerHeight,
+    // backgroundColor: "gray",
+    width: "100%",
   },
   listItemButton: {
-    background: '#E9EEF3',
-    width: '90%',
-    borderRadius: 3,
-    transition: '0.05s',
-    '&.Mui-selected': {
-      backgroundColor: '#5DADE2',
-    }
+    // background: "#E9EEF3",
+    width: "100%",
+    // borderRadius: 3,
+    transition: "0.05s",
+    "&.Mui-selected": {
+      backgroundColor: "#5DADE2",
+    },
   },
   listItemText: {
     fontWeight: theme.typography.fontWeightMedium,
@@ -42,21 +61,20 @@ const useStyles = makeStyles({
 function DrawerList1() {
   const { token } = useAuthContext();
   const theme = useTheme();
-  const isNotPC = useMediaQuery(theme.breakpoints.down('md'));
+  const isNotPC = useMediaQuery(theme.breakpoints.down("md"));
   const classes = useStyles();
   const location = useLocation();
   const [file, setFile] = useState([]);
   const [subFile, setSubFile] = useState([]);
   const [col, setcol] = useState(-1);
 
-
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get("http://localhost:3001/read/new", {
           headers: {
-            'Authorization': `Bearer ${token}`
-          }
+            Authorization: `Bearer ${token}`,
+          },
         });
         // console.log("res.data.department:", response.data.department);
         setFile(response.data.department || []);
@@ -68,20 +86,24 @@ function DrawerList1() {
     fetchData();
   }, [token]);
 
-
   const handleClick = async (department, index) => {
     console.log(department);
 
     // Fetch subdepartments for the selected department
     const fetchSubDepartments = async () => {
       try {
-        const response = await axios.get(`http://localhost:3001/read/subdepartments/${department}`, {
-          headers: {
-            'Authorization': `Bearer ${token}`
+        const response = await axios.get(
+          `http://localhost:3001/read/subdepartments/${department}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
           }
-        });
-        console.log("response.data.subDepartment " + response.data.subDepartment)
-        setSubFile(response.data.subDepartment)
+        );
+        console.log(
+          "response.data.subDepartment " + response.data.subDepartment
+        );
+        setSubFile(response.data.subDepartment);
         // setSubDepartments(response.data.subDepartments || []);
         // setSelectedDepartment(department); // Set selected department
       } catch (error) {
@@ -94,42 +116,53 @@ function DrawerList1() {
     setcol(col === index ? -1 : index);
   };
 
-   // Get unique categories
-   const uniqueDepartment = [...new Set(file.map(book => book.department))];
-   const uniqueSubDepartment = [...new Set(subFile.map(book => book.subDepartment))];
-//    console.log("uniqueSubDepartment " + uniqueSubDepartment)
+  // Get unique categories
+  const uniqueDepartment = [...new Set(file.map((book) => book.department))];
+  const uniqueSubDepartment = [
+    ...new Set(subFile.map((book) => book.subDepartment)),
+  ];
+  //    console.log("uniqueSubDepartment " + uniqueSubDepartment)
 
   return (
-    <Container className={classes.root}>
- 
+    <Box className={classes.root}>
       <Drawer
         className={classes.drawer}
-        variant='permanent'
-        anchor='left'
+        variant="permanent"
+        anchor="left"
         sx={{
-          '& .MuiDrawer-paper': {
-            width: isNotPC ? '45%' : '27%',
+          "& .MuiDrawer-paper": {
+            width: isNotPC ? "30%" : "15%",
             height: drawerHeight,
-            marginTop: '105px',
-            paddingTop: '15px',
-          }
+            marginTop: "105px",
+            // paddingTop: "15px",
+            boxShadow: 2,
+          },
         }}
-      > 
-        <Container className={classes.drawContainer}>
-
-          <Typography variant='h6'>
-            List of Department
-          </Typography>
+      >
+        <Box className={classes.drawBox}>
+          <Typography variant="h5">List of Department</Typography>
           <List>
             {uniqueDepartment.map((department, index) => {
-            //   const itemName = Array.isArray(item.department) && item.department.length > 0 ? item.department[0] : item.department;
-            const itemName = department
-            const pathName = decodeURIComponent(location.pathname);
+              //   const itemName = Array.isArray(item.department) && item.department.length > 0 ? item.department[0] : item.department;
+              const itemName = department;
+              const pathName = decodeURIComponent(location.pathname);
               const isSelected = pathName.includes(itemName);
+
+              <Divider component="li" />;
 
               return (
                 <ListItem
-                  sx={{ display: 'flex', flexDirection: 'column', alignItems: 'start', width: '100%', backgroundColor: '' }}
+                  divider={3}
+                  disablePadding
+                  // aria-disabled
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "start",
+                    width: "100%",
+                    backgroundColor: "",
+                    paddingBottom: "3px",
+                  }}
                   key={index}
                 >
                   <ListItemButton
@@ -137,58 +170,71 @@ function DrawerList1() {
                     selected={isSelected}
                     onClick={() => handleClick(department, index)}
                     sx={{
-                      transition: 'none',
-                        background: '#E9EEF3',
-                        width: '100%',
-                        borderRadius: 3,
-                        transition: '0.05s',
-                        '&.Mui-selected': {
-                          backgroundColor: '#5DADE2 ',
-                          // color: 'white',
-                          ":hover" : {
-                        backgroundColor: '#5DADE2',
-                        opacity: '0.5'
-                      }
-                        },
-                        ":hover" : {
-                          backgroundColor: '#5DADE2',
-                          opacity: '0.5'
-                        }
-                        
-                      }}
-                  >
-                    <ListItemText primary={department} classes={{ primary: classes.listItemText }} />
+                      // transition: "none",
+                      background: "#E9EEF3",
+                      transition: "0s",
+                      transform: "translateX(0) rotate(0)",
 
+                      // width: "90%",
+                      // borderRadius: 3,
+                      // transition: "0.05s",
+                      "&.Mui-selected": {
+                        backgroundColor: "#5DADE2 ",
+                        // color: 'white',
+                        ":hover": {
+                          backgroundColor: "#5DADE2",
+                          opacity: "0.5",
+                        },
+                      },
+                      ":hover": {
+                        backgroundColor: "#5DADE2",
+                        opacity: "0.5",
+                      },
+                    }}
+                  >
+                    <ListItemText
+                      primary={department}
+                      classes={{ primary: classes.listItemText }}
+                    />
                   </ListItemButton>
 
-                  <Stack sx={{width: '92%', backgroundColor: ''}}>
+                  <Stack sx={{ width: "100%", backgroundColor: "" }}>
                     <Collapse in={col === index} timeout="auto" unmountOnExit>
-                      <List sx={{width: '100%', backgroundColor: ''}}>
-                        {uniqueSubDepartment !== null && uniqueSubDepartment.map((subDepartment, index) => (
-                          <ListItem
-                            key={index}
-                            button
-                            component={Link}
-                            to={`/one/${department}/${subDepartment}`}
-                            sx={{  ml: 3,mb:0.2, borderRadius: '5px' , backgroundColor: '#E9EEF3', borderRadius: 3}}
-                          >
-                            <ListItemText primary={subDepartment} classes={{ primary: classes.subListItemText }} />
+                      <List sx={{ width: "100%", backgroundColor: "" }}>
+                        {uniqueSubDepartment !== null &&
+                          uniqueSubDepartment.map((subDepartment, index) => (
+                            <ListItem
+                              key={index}
+                              button
+                              component={Link}
+                              to={`/one/${department}/${subDepartment}`}
+                              sx={{
+                                ml: 2,
+                                mb: 0.2,
+                                mr: 1,
+                                // borderRadius: "5px",
+                                backgroundColor: "#E9EEF3",
+                                // borderRadius: 3,
+                              }}
+                            >
+                              <ListItemText
+                                primary={subDepartment}
+                                classes={{ primary: classes.subListItemText }}
+                              />
 
-                            {/* <ListItemText primary={subDepartment} sx={{ background: '', borderRadius: '5px',  }} /> */}
-                          </ListItem>
-                        ))}
+                              {/* <ListItemText primary={subDepartment} sx={{ background: '', borderRadius: '5px',  }} /> */}
+                            </ListItem>
+                          ))}
                       </List>
                     </Collapse>
                   </Stack>
-
-
                 </ListItem>
               );
             })}
           </List>
-        </Container>
+        </Box>
       </Drawer>
-    </Container>
+    </Box>
   );
 }
 
--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,23 +1,23 @@
-import { makeStyles } from '@material-ui/styles';
-import { useTheme } from '@mui/material/styles';  // Use useTheme from @mui/material/styles
+import { makeStyles } from "@material-ui/styles";
+import { useTheme } from "@mui/material/styles"; // Use useTheme from @mui/material/styles
 
-import React, { useContext } from 'react';
-import { Box, Stack, Typography } from '@mui/material';
-import DrawerList from './DrawerList';
-import Navbar from './Navbar';
-import Announcement from './Announcement';
-import AppContext from '../context/AppContext';
-import useMediaQuery from '@mui/material/useMediaQuery';
-import DrawerList1 from './DrawerList2';
-import Navbar2 from './Navbar2';
+import React, { useContext } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import DrawerList from "./DrawerList";
+import Navbar from "./Navbar";
+import Announcement from "./Announcement";
+import AppContext from "../context/AppContext";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import DrawerList1 from "./DrawerList2";
+import Navbar2 from "./Navbar2";
 
 const useStyles = makeStyles((theme) => ({
   page: {
-    width: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
+    width: "100%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
     // [theme.breakpoints.down('sm')]: {
     //   backgroundColor: 'lightblue', // Mobile
     // },
@@ -27,56 +27,55 @@ const useStyles = makeStyles((theme) => ({
     // [theme.breakpoints.up('md')]: {
     //   backgroundColor: 'lightcoral', // PC
     // },
-    
   },
+
   layout: {
-    width: '100%',
-    display: 'flex',
+    width: "100%",
+    display: "flex",
     flexGrow: 1,
   },
+
   drawer: {
     // [theme.breakpoints.up('md')]: {
     //   backgroundColor: 'lightcoral', // PC
     // },
     // display: 'none',
-    backgroundColor: 'gray'
+    // backgroundColor: "gray",
   },
+  
   announcement: {
-      // display: 'none', 
-      backgroundColor: 'gray',
-      height: 'auto'
-  }
+    // display: 'none',
+    backgroundColor: "gray",
+    height: "auto",
+  },
 }));
 
-function Layout({ children }) { 
+function Layout({ children }) {
   const { navbar, nameContext } = useContext(AppContext);
   const classes = useStyles();
   const theme = useTheme();
 
-  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
-  console.log("isMobile " + isMobile)
-  const isTablet = useMediaQuery(theme.breakpoints.between('sm', 'md'));
-  const isPC = useMediaQuery(theme.breakpoints.up('md'));
-  const aboveMobile = useMediaQuery(theme.breakpoints.up('sm'));
-  const notPC = useMediaQuery(theme.breakpoints.down('md'));
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+  console.log("isMobile " + isMobile);
+  const isTablet = useMediaQuery(theme.breakpoints.between("sm", "md"));
+  const isPC = useMediaQuery(theme.breakpoints.up("md"));
+  const aboveMobile = useMediaQuery(theme.breakpoints.up("sm"));
+  const notPC = useMediaQuery(theme.breakpoints.down("md"));
   // console.log("useMediaQuery is pc " + isPC);
 
   const announcementStyles = {
-    display: isMobile ? 'none' : 'block',
+    display: isMobile ? "none" : "block",
   };
 
   return (
     <Stack className={classes.page}>
       {<Navbar />}
-      
-      <Box className={classes.layout}>
-        {children}
-      </Box>
+
+      <Box className={classes.layout}>{children}</Box>
       {nameContext && <DrawerList1 className={classes.drawer} />}
-      { nameContext && <Announcement /> }
+      {nameContext && <Announcement />}
     </Stack>
   );
 }
-
 
 export default Layout;
