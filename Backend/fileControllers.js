eba374a4223a5440be7b2f43b2a7552c86595234
Abdi Bacha <contactabdi.bacha@gmail.com>
2024-06-20 21:53:10 +0300

    update

--- Backend/fileControllers.js
+++ Backend/fileControllers.js
@@ -179,7 +179,6 @@ const getSubDepartment2 = asyncErrorHandler(async (req, res) => {
   });
 });
 
-
 const getFiles = async (req, res) => {
   const item = req.params.item;
   const item2 = req.params.item2;
